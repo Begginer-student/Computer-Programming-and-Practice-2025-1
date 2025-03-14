8828925a1c3864f36a2596509a245b283b7e54d5
Begginer-student <emqdlfqnrP@gmail.com>
2025-03-14 14:23:47 +0900

    Update main.c

--- main.c
+++ main.c
@@ -10,11 +10,28 @@
  */
 
 /* five는 int 타입의 값인 5를 반환하는 함수입니다. */
+
+#include<stdio.h>
 int five() {
 	return 5;
 }
 
 int main() {
 	/* To Do: five()를 호출한 결과를 반환해보세요 */
+	printf("%d", five());
 	return 0;
 }
+// 위 코드는 vs code는 printf가 존재하지 않으면 출력되지 않아서 vs code에 맞게 코드를 변형해보았습니다.
+
+int five()
+{
+	int five();
+	{
+		return 5;
+	}
+}
+int main()
+{
+	return five();
+}
+// 위 코드는 vs용 정답코드로 만들었습니다.
